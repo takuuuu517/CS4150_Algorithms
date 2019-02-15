97b5e84446f0f916ac81c09b5969540a4cc01ea7
takuuuu517 <ryoryoryoasa@gmail.com>
2019-02-14 18:31:22 -0700

    rumor mill done

--- Rumor_Mill/src/com/company/Main.java
+++ Rumor_Mill/src/com/company/Main.java
@@ -8,16 +8,15 @@ public class Main {
     private static HashMap<String, ArrayList<String>> friendslist = new HashMap<>(); // main list
     private static String[] startingStudents;
     private static int starternum;
-    
-    
-    private static HashSet<String> visitedStudents = new HashSet<>();
 
-    private static ArrayList<String> pre_time = new ArrayList<>();
-    private static ArrayList<String> post_time = new ArrayList<>();
+//    private static HashSet<String> visitedStudents = new HashSet<>();
+    private static HashMap<String, Integer> dist = new HashMap<>();
+    private static HashMap<String, String> prev = new HashMap<>();
 
     private static int studentnum;
     private static int connectionNum;
 
+    private static final int inf = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -35,51 +34,88 @@ public class Main {
             String s1 = scan.next();
             String s2 = scan.next();
             friendslist.get(s1).add(s2);
+            friendslist.get(s2).add(s1);
+
         }
-
-
         starternum = scan.nextInt();
         startingStudents = new String[starternum];
 
-        topological_sort(friendslist,visitedStudents);
-
-
         for(int i = 0; i < starternum; i++) // get all of the students who start the rumor.
-            startingStudents[i] = scan.next();
-
-        System.out.print("");
-    }
-
-
-
-
-    static void topological_sort(HashMap<String, ArrayList<String>> mlist, HashSet<String> visitedlist)
-    {
-        visitedlist.clear();
-        pre_time.clear();
-        post_time.clear();
-
-        for(Map.Entry<String, ArrayList<String>> entry : mlist.entrySet())
         {
-            if(!visitedlist.contains(entry.getKey()))
-                explore(mlist, visitedlist, entry.getKey());
+            startingStudents[i] = scan.next();
+            bfs(friendslist,startingStudents[i]);
+            System.out.println(sortMapbyValue(dist));
         }
     }
+
+
+    static void bfs (HashMap<String, ArrayList<String>> g, String start)
+    {
+        //initialize
+        for(Map.Entry<String, ArrayList<String>> entry : g.entrySet())
+        {
+            dist.put(entry.getKey(),inf);
+            prev.put(entry.getKey(),null);
+        }
+        dist.put(start,0);
+
+        Queue<String> q = new ArrayDeque<>();
+        q.add(start);
+
+        while (!q.isEmpty())
+        {
+            String u = q.remove();
+            for(String v: friendslist.get(u))
+                if(dist.get(v) == inf)
+                {
+                    q.add(v);
+                    dist.put(v, add(dist.get(u),1));
+                    prev.put(v,u);
+                }
+        }
+    }
+
 
     /**
-     * @param mlist
-     * @param visitedlist
-     * @param city
+     * simple add method to avoid over flow.
+     * @param a
+     * @param b
+     * @return return a+b normally. if a or b is max int then, return max int.
      */
-    static void explore(HashMap<String, ArrayList<String>> mlist, HashSet<String> visitedlist, String city)
+    private static int add(int a, int b)
     {
-        visitedlist.add(city);
-        pre_time.add(city);
-        for(int i = 0; i < mlist.get(city).size(); i++)
+        if(a == inf || b == inf )
+            return inf;
+        return a+b;
+    }
+
+
+    /**
+     *
+     * @param distmap
+     * @return string of of the map in sorted order separated by a space
+     */
+    private static String sortMapbyValue(HashMap<String, Integer> distmap)
+    {
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(distmap.entrySet()); // make a list from the map.
+        Collections.sort(list, new comparator()); // sort the map using the comparator.
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < list.size(); i++)
         {
-            if(!visitedlist.contains(mlist.get(city).get(i)))
-                explore(mlist,visitedlist, mlist.get(city).get(i));
+            sb.append(list.get(i).getKey());
+            sb.append(" ");
         }
-        post_time.add(0,city);
+        return sb.toString();
+    }
+}
+
+/**
+ * compare the distance from the start first, then if the distances are the same compare the student names.
+ */
+class comparator implements Comparator<Map.Entry<String, Integer>> {
+    public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+        if(o1.getValue().compareTo(o2.getValue()) == 0)
+            return o1.getKey().compareTo(o2.getKey());
+        return o1.getValue().compareTo(o2.getValue());
     }
 }
